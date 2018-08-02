99bdf103410faed153e7f92ab4515c14608cdd7e
Jonathan Luk <jluk87@gmail.com>
2018-08-02 18:48:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
   attr_accessor :name, :grade
   attr_reader :id
@@ -40,7 +42,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-
+    binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
   end
