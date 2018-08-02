149f40468a0a94dd1b25bca6877b97c9ba66e659
Jonathan Luk <jluk87@gmail.com>
2018-08-02 18:26:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,20 @@
 class Student
   attr_accessor :name, :grade, :id
-  
+
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     @id = id
+
+  end
+
+  def save
+    sql = <<- SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
 
   end
 
